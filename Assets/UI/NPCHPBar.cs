81517ae5aff9f479612a14de601c2dd57b9e2467
Kevin Bryan <kevincbryan@gmail.com>
2020-01-08 12:20:21 -0800

    Adding Croc Health Bar - In progress

---
+++ Assets/UI/NPCHPBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCHPBar : MonoBehaviour
+{
+
+    public HP playerHP;
+    public Image healthbar;
+    private float healthPercentage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthPercentage = (float)playerHP.hp / (float)playerHP.hpMax;
+        healthbar.fillAmount = healthPercentage;
+    }
+}
