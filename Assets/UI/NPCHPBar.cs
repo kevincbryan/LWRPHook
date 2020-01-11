e01c21e390aeb126fea0a6ea7c1f53681a788ac3
Kevin <kevincbryan@gmail.com>
2020-01-11 15:12:47 -0800

    Hid NPC HPBars until damaged

--- Assets/UI/NPCHPBar.cs
+++ Assets/UI/NPCHPBar.cs
@@ -8,6 +8,7 @@ public class NPCHPBar : MonoBehaviour
 
     public HP npcHP;
     public Image healthbar;
+    public Image backHealth;
     private float healthPercentage;
 
     // Start is called before the first frame update
@@ -19,6 +20,18 @@ public class NPCHPBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (healthbar.enabled == false )
+        {
+
+            if ( npcHP.hp != npcHP.hpMax)
+            {
+                healthbar.enabled = true;
+                backHealth.enabled = true;
+                Debug.Log("This should run onceish");
+            }
+            
+
+        }
         healthPercentage = (float)npcHP.hp / (float)npcHP.hpMax;
         healthbar.fillAmount = healthPercentage;
     }
